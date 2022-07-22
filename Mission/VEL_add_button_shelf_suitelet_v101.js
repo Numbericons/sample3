31dce23856fc485b3b501dc566f41add781dcfe1
Numbericons <zachjoliver@gmail.com>
2022-07-22 01:09:35 -0700

    Add working version of mission add item shelf label button

--- Mission/VEL_add_button_shelf_suitelet_v101.js
+++ Mission/VEL_add_button_shelf_suitelet_v101.js
@@ -22,25 +22,20 @@ define(['N/file', 'N/record', 'N/render', 'N/search'],
          * @since 2015.2
          */
         const onRequest = (scriptContext) => {
-
             var logTitle = 'onRequest';
             var request = scriptContext.request;
             var recId = request.parameters.id;
-          	var recType = request.parameters.type;
             log.debug(logTitle, 'Id: ' + recId);
-            log.debug(logTitle, 'Whats the type: ' + recType);
 	
             var item = record.load({
                 type: record.Type.INVENTORY_ITEM,
                 id: recId
             });
 
-            log.debug('record: ', item)
-
             // create PDF
             var xmlFile = file.load({
                 id: '3294508'
-            }); // internal id of XML file
+            });
             var xmlFileValue = xmlFile.getContents();
 
             log.error('xmlFileValue: ', xmlFileValue);
@@ -60,17 +55,112 @@ define(['N/file', 'N/record', 'N/render', 'N/search'],
             scriptContext.response.renderPdf(xmlString);
         }
 
+        const getUnits = (item_record) => {
+            var units = item_record.getValue('saleunit');
+            
+            var unitRec = record.load({
+                type: record.Type.UNITS_TYPE,
+                id: units
+            });
+
+            var unitText = unitRec.getSublistValue({
+                     sublistId: 'uom',
+                     fieldId: 'unitname',
+                     line: 0
+            });
+
+            return unitText;
+        }
+
+        const getQuantities = (item_record) => {
+            const quantities = ["pricequantity1","pricequantity2", "pricequantity3", "pricequantity4"];
+            var priceQuants = {};
+
+            for (let k=0; k<quantities.length; k++){ 
+                var priceBreak = item_record.getValue(quantities[k]);
+                priceQuants[quantities[k]] = priceBreak;
+            }
+
+            return priceQuants;
+        }
+
+        const getPrice = (item_record) => {
+            const priceCount = item_record.getLineCount({ sublistId: 'price' });
+
+            const priceQuantities = getQuantities(item_record);
+            const prices = ["price_1_", "price_2_", "price_3_", "price_4_"];
+            
+            for (let k=0; k<priceCount; k++){
+                var price = item_record.getSublistValue({
+                         sublistId: 'price',
+                         fieldId: 'pricelevelname',
+                         line: k
+                });
+
+                log.debug(price, price);
+                if (price !== "Retail Price") continue;
+
+                var priceRet = {};
+                priceRet.retailPrice = item_record.getSublistValue({
+                        sublistId: 'price',
+                        fieldId: 'price_1_',
+                        line: k
+                });
+                log.debug('Price ret with retail:', priceRet);
+
+                for (let i=1; i<prices.length; i++){
+
+                    var currPrice = item_record.getSublistValue({
+                        sublistId: 'price',
+                        fieldId: prices[i],
+                        line: k
+                    });
+                    if (!currPrice) continue;
+
+                    priceRet.qtyPrice = currPrice;
+                    priceRet.qtyBreak = priceQuantities[`pricequantity${i+1}`];
+
+                    log.debug('priceRet: ', priceRet);
+                    return priceRet;
+                }
+
+                return priceRet;
+            }
+        }
+
+        const setItem = (itemRec) => {
+            var newItem = {};
+
+            newItem.id = itemRec.getValue('itemid');
+            newItem.name = itemRec.getValue('name');
+            newItem.display = itemRec.getValue('displayname');
+            newItem.unitText = getUnits(itemRec);
+            newItem.vendor = itemRec.getValue('vendorname');
+            newItem.pricing = getPrice(itemRec);
+
+            return newItem;
+        }
+
         const createText = (itemRec) => {
             var text =  '<tr>';
-            var test = 'test'
-            const display = itemRec.getValue('displayname');
-            const units = itemRec.getValue('saleunit');
-
+            item = setItem(itemRec);
             
-            text += `<td colspan="12" height= "60px" style="font-size: 19pt;">${display}</td>`;
-            text += '</tr>';
-            text += '<tr>';
-            text += `<td colspan="12" height= "60px" style="font-size: 19pt;">${units}</td>`;
+            text += `<td colspan="12" height= "60px" style="font-size: 19pt;">${item.display}</td>`;
+            text += '</tr><tr>';
+            text += `<td colspan="12" height= "60px" style="font-size: 19pt;">${item.pricing.retailPrice}</td>`;
+            text += '</tr><tr>';
+            text += `<td colspan="12" height= "60px" style="font-size: 19pt;">${item.unitText}</td>`;
+            text += '</tr><tr>';
+
+            if (item.pricing.qtyPrice) {
+                text += `<td colspan="12" height= "60px" style="font-size: 19pt;">Buy ${item.pricing.qtyBreak} for $${item.pricing.qtyPrice} ${item.unitText}</td>`;
+            }
+            text += '</tr><tr>';
+            text += `<td colspan="12" height= "60px" style="font-size: 19pt;">${item.vendor}</td>`;
+            text += '</tr><tr>';
+            text += `<td colspan="12" height= "60px" style="font-size: 19pt;"><barcode codetype="code128"  showtext="false" value="${item.id}"/></td>`;
+            text += '</tr><tr>';
+            text += `<td colspan="12" height= "60px" style="font-size: 19pt;">${item.id}</td>`;
             text += '</tr>';
 
             return text;
